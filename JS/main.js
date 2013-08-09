b82626a0f55185a4d8121bf5ba4960d5aba890f5
Pedro Rodriguez <pjrod@fullsail.edu>
2013-08-08 22:17:43 -0400

    made minor changes to index and js file

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,17 @@
 //Pedro J Rodriguez
 //ASD AUG 2013
 //main.js file
+
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+	
+	
+});	
+
+var parseMyForm = function(data){
+	// use of form data here;
+	console.log(data);
+	
+	
+};
